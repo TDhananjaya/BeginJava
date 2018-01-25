93cd934d73ce9b6750bbcc27091c6d7ecb636e62
TDhananjaya <tharindudhananjaya1122@gmail.com>
2018-01-25 23:42:21 +0530

    Print Word

---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Dhana
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
